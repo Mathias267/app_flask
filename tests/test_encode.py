d6123427468bc45d41ae7e1daef18ac1c92c89d1
Mathias267 <mathiasmoreiraisrael@gmail.com>
2024-11-07 09:30:53 -0300

    conclui.app

--- tests/test_encode.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.encode import encode
-
-
-flask_app = create_app()
-
-def test_encode_function():
-    encoded_message = encode("ABCD", 2)
-    assert encoded_message == "CDEF"
